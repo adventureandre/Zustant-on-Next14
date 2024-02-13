4b0b4aa6403e23d9f2e374b6e67e9498fe8fc4a5
Andre Luiz <adventureandre@hotmail.com>
2024-02-13 01:13:01 -0300

    Removed Suspense

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { usePostStore } from "../../../../store/postStore"
-import { Suspense } from "react";
 
 export default function Post({ params }: { params: { id: string } }) {
 
